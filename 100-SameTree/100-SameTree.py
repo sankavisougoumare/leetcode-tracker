60ca9a31530045c74bb0e40ecd59e9b1ee8e13f4
sankavisougoumare <sankavi021106@gmail.com>
2026-07-23 11:00:10 +0530

    Create 100-SameTree

---
+++ 100-SameTree/100-SameTree.py
@@ -0,0 +1,13 @@
+# Last updated: 23/07/2026, 11:00:09
+class Solution:
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+
+        if not p or not q:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
