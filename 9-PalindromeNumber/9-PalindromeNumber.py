9702d4f048feb46d27d7b5e8a598d1dc03514524
sankavisougoumare <sankavi021106@gmail.com>
2026-07-23 11:00:46 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -0,0 +1,16 @@
+# Last updated: 23/07/2026, 11:00:45
+class Solution:
+    def isPalindrome(self, x):
+
+        if x < 0:
+            return False
+
+        original = x
+        reverse = 0
+
+        while x > 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x //= 10
+
+        return original == reverse
