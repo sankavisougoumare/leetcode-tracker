48d031cf2cd0731b7a6a0acdda68f95ca7e9e93f
sankavisougoumare <sankavi021106@gmail.com>
2026-07-23 10:59:30 +0530

    Create 187-RepeatedDnaSequences

---
+++ 187-RepeatedDnaSequences/187-RepeatedDnaSequences.java
@@ -0,0 +1,19 @@
+// Last updated: 23/07/2026, 10:59:29
+import java.util.*;
+
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> seen = new HashSet<>();
+        Set<String> repeated = new HashSet<>();
+
+        for (int i = 0; i <= s.length() - 10; i++) {
+            String sub = s.substring(i, i + 10);
+
+            if (!seen.add(sub)) {
+                repeated.add(sub);
+            }
+        }
+
+        return new ArrayList<>(repeated);
+    }
+}
